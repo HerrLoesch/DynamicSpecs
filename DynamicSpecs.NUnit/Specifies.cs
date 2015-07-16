4c2769f49ce3bc2bc7f4e4dfaf1239ca93a997af
Hendrik Lösch <loeschhendrik@gmail.com>
2015-07-16 10:24:12 +0200

    Added NUnit Support.

---
+++ DynamicSpecs.NUnit/Specifies.cs
@@ -0,0 +1,28 @@
+﻿namespace DynamicSpecs.NUnit
+{
+    using DynamicSpecs.AutoFacItEasy;
+    using DynamicSpecs.Core;
+
+    using global::NUnit.Framework;
+
+    [TestFixture]
+    public abstract class Specifies<T> : SpecifiesBaseState<T>
+    {
+        private readonly TypeRegistration typeRegistration;
+
+        protected Specifies()
+        {
+            this.typeRegistration = new TypeRegistration();
+        }
+
+        protected override IRegisterTypes GetTypeRegistration()
+        {
+            return this.typeRegistration;
+        }
+
+        protected override IResolveTypes GetTypeResolver()
+        {
+            return this.typeRegistration;
+        }
+    }
+}
