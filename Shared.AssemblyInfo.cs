f5286e0ae258eaf07ec662486de2d1f31e8ec7cc
Christian Harlass <christian.harlass@gmail.com>
2015-09-06 19:32:41 +0200

    nuget

---
+++ Shared.AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//[assembly: AssemblyTitle("DynamicSpecs.MSTest")]
+//[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hendrik Loesch")]
+[assembly: AssemblyProduct("DynamicSpecs")]
+[assembly: AssemblyCopyright("Copyright © 2015 Hendrik Loesch")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
