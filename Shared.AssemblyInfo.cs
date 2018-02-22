0a40781586263ef6c3c3f913392dc2b0e4be0b64
Hendrik Lösch <loeschhendrik@gmail.com>
2018-02-22 17:31:26 +0100

    Prepared Version 2.3

--- Shared.AssemblyInfo.cs
+++ Shared.AssemblyInfo.cs
@@ -3,9 +3,9 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hendrik Loesch")]
 [assembly: AssemblyProduct("DynamicSpecs")]
-[assembly: AssemblyCopyright("Copyright © 2017 Hendrik Loesch")]
+[assembly: AssemblyCopyright("Copyright © 2018 Hendrik Loesch")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
