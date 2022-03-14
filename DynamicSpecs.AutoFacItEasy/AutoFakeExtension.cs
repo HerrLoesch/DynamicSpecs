69be420e5090d0a84ef0fceaf2d60779388cdeba
Nicolas Kis <nicolas.kis.ext@zeiss.com>
2022-03-14 13:58:02 +0100

    Adding optimized implementation of Autofake

---
+++ DynamicSpecs.AutoFacItEasy/AutoFakeExtension.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
+using Autofac.Extras.FakeItEasy;
+
+namespace DynamicSpecs.AutoFacItEasy
+{
+    /// <summary>
+    /// Optimized implementation of AutoFake class that allows
+    /// a batch registration of various types and instances
+    /// with their corresponding order
+    /// </summary>
+    public class AutoFakeImpl : AutoFake
+    {
+        private bool _disposed;
+
+        private readonly Stack<ILifetimeScope> _scopes = new Stack<ILifetimeScope>();
+
+        private ILifetimeScope _currentScope;
+
+        public AutoFakeImpl(bool strict = false, bool callsBaseMethods = false, Action<object> configureFake = null, ContainerBuilder builder = null, Action<ContainerBuilder> configureAction = null) 
+            : base(strict, callsBaseMethods, configureFake, builder, configureAction)
+        {
+            base.Container.ChildLifetimeScopeBeginning += Container_ChildLifetimeScopeBeginning;
+            _currentScope = base.Container.BeginLifetimeScope();
+        }
+        /// <summary>
+        /// Resolve the specified type in the container (register it if needed).
+        /// </summary>
+        /// <typeparam name="T">The type of the service.</typeparam>
+        /// <param name="parameters">Optional parameters.</param>
+        /// <returns>The service.</returns>
+        new public T Resolve<T>(params Parameter[] parameters) => this._currentScope.Resolve<T>(parameters);
+
+        /// <summary>
+        /// Resolve the specified type in the container (register it if needed).
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The implementation of the service.</typeparam>
+        /// <param name="parameters">Optional parameters.</param>
+        /// <returns>The service.</returns>
+        new public TService Provide<TService, TImplementation>(params Parameter[] parameters)
+        {
+            this._currentScope.BeginLifetimeScope(b =>
+            {
+                b.RegisterType<TImplementation>().As<TService>().InstancePerLifetimeScope();
+            });
+
+            return this._currentScope.Resolve<TService>(parameters);
+        }
+
+        /// <summary>
+        /// Resolve the specified type in the container (register specified instance if needed).
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <param name="instance">The instance to register if needed.</param>
+        /// <returns>The instance resolved from container.</returns>
+        new public TService Provide<TService>(TService instance)
+            where TService : class
+        {
+            this._currentScope.BeginLifetimeScope(b =>
+            {
+                b.Register(c => instance).InstancePerLifetimeScope();
+            });
+
+            return this._currentScope.Resolve<TService>();
+        }
+        /// <summary>
+        /// Bulk Register of types.
+        /// </summary>
+        /// <typeparam name="TSource">Source type which is registered.</typeparam>
+        /// <typeparam name="TTarget">Target type as which the source type is used.</typeparam>
+        /// <typeparam name="Instance">Instance of the source that will be used as service.</typeparam>
+        public void BatchRegister(List<(Type, Type, object)> RegisterTypes)
+        {
+            this._currentScope.BeginLifetimeScope(b =>
+            {
+                foreach (var action in RegisterTypes)
+                {
+                    if (action.Item3 != default(object))
+                        b.Register(c => action.Item3).As(action.Item1).InstancePerLifetimeScope();
+                    else
+                        b.RegisterType(action.Item1).As(action.Item2).InstancePerLifetimeScope();
+                }
+            });
+
+            foreach (var action in RegisterTypes)
+            {
+                this._currentScope.Resolve(action.Item2);
+            }
+        }
+        public void Container_ChildLifetimeScopeBeginning(object sender, Autofac.Core.Lifetime.LifetimeScopeBeginningEventArgs e)
+        {
+            this._currentScope = e.LifetimeScope;
+            this._currentScope.ChildLifetimeScopeBeginning += Container_ChildLifetimeScopeBeginning;
+            this._scopes.Push(this._currentScope);
+        }
+
+        /// <summary>
+        /// Handles disposal of managed and unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <see langword="true" /> to dispose of managed resources (during a manual execution
+        /// of <see cref="AutoFake.Dispose()"/>); or
+        /// <see langword="false" /> if this is getting run as part of finalization where
+        /// managed resources may have already been cleaned up.
+        /// </param>
+        new protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    while (this._scopes.Count > 0)
+                        this._scopes.Pop().Dispose();
+
+                    this.Container.Dispose();
+                }
+                this._disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="AutoFakeImpl"/> class.
+        /// </summary>
+        [SecuritySafeCritical]
+        ~AutoFakeImpl() => this.Dispose(false);
+        /// <summary>
+        /// Disposes internal container.
+        /// </summary>
+        [SecuritySafeCritical]
+        new public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- DynamicSpecs.AutoFacItEasy/TypeRegistry.cs
+++ DynamicSpecs.AutoFacItEasy/TypeRegistry.cs
@@ -1,25 +1,26 @@
 namespace DynamicSpecs.AutoFacItEasy
 {
     using System;
-
-    using Autofac.Extras.FakeItEasy;
+    using System.Collections.Generic;
+    using Autofac;
     using DynamicSpecs.Core;
-
     /// <summary>
     /// Container holding all registered types.
     /// </summary>
     public class TypeRegistry : IRegisterTypes, IResolveTypes
     {
-        private readonly AutoFake fakeContainer;
+        private AutoFakeImpl fakeContainer;
+        private bool disposedValue;
+        private readonly Stack<ILifetimeScope> _scopes = new Stack<ILifetimeScope>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TypeRegistry"/> class.
         /// </summary>
         public TypeRegistry()
         {
-            this.fakeContainer = new AutoFake();
+            this.fakeContainer = new AutoFakeImpl(builder: new ContainerBuilder());
         }
-        
+
         /// <summary>
         /// Registers an instance as a target type.
         /// </summary>
@@ -52,6 +53,17 @@ namespace DynamicSpecs.AutoFacItEasy
         }
 
         /// <summary>
+        /// Bulk Register of types.
+        /// </summary>
+        /// <typeparam name="TSource">Source type which is registered.</typeparam>
+        /// <typeparam name="TTarget">Target type as which the source type is used.</typeparam>
+        /// <typeparam name="Instance">Instance of the source that will be used as service.</typeparam>
+        public void BatchRegister(List<(Type, Type, object)> RegisterTypes)
+        {
+            this.fakeContainer.BatchRegister(RegisterTypes);
+        }
+
+        /// <summary>
         /// Resolves an instance of the given type.
         /// </summary>
         /// <typeparam name="T">Type to resolve.</typeparam>
@@ -59,6 +71,35 @@ namespace DynamicSpecs.AutoFacItEasy
         public T Resolve<T>() where T : class
         {
             return this.fakeContainer.Resolve<T>();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this.fakeContainer?.Dispose();
+                    this.fakeContainer = null;
+                }
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~TypeRegistry()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
